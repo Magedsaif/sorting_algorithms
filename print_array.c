0f20778f522bf86bd411e6e8bc2c97ed885e7494
abeermosaad <abeermosaad69@gmail.com>
2023-07-20 18:35:59 +0300

    print_array is added

---
+++ print_array.c
@@ -0,0 +1,62 @@
+#include <stdio.h>
+#include "sort.h"
+
+/**
+ * print_list - Prints a list of integers
+ *
+ * @list: The list to be printed
+ */
+void print_list(const listint_t *list)
+{
+    int i;
+
+    i = 0;
+    while (list)
+    {
+        if (i > 0)
+            printf(", ");
+        printf("%d", list->n);
+        ++i;
+        list = list->next;
+    }
+    printf("\n");
+}
+
+
+/**
+ * print_array - Prints an array of integers
+ *
+ * @array: The array to be printed
+ * @size: Number of elements in @array
+ */
+void print_array(const int *array, size_t size)
+{
+    size_t i;
+
+    i = 0;
+    while (array && i < size)
+    {
+        if (i > 0)
+            printf(", ");
+        printf("%d", array[i]);
+        ++i;
+    }
+    printf("\n");
+}
+
+/**
+ * swap - swap two numbers
+ *
+ * @a: first num
+ * @b: second num
+ */
+int* swap(int array[], int idx1, int idx2)
+{
+	int temp = 0;
+
+	temp = array[idx1];
+	array[idx1] = array[idx2];
+	array[idx2] = temp;
+
+    return (array);
+}
