4baac0b8661eecaaa564337f5cdd00965228c746
abeermosaad <abeermosaad69@gmail.com>
2023-07-21 00:13:47 +0300

    betty is happy now :)

--- sort.h
+++ sort.h
@@ -14,13 +14,13 @@
  */
 typedef struct listint_s
 {
-    const int n;
-    struct listint_s *prev;
-    struct listint_s *next;
+	const int n;
+	struct listint_s *prev;
+	struct listint_s *next;
 } listint_t;
 
 void bubble_sort(int *array, size_t size);
-int* swap(int array[], int idx1, int idx2);
+int	*swap(int array[], int idx1, int idx2);
 void print_array(const int *array, size_t size);
 void print_list(const listint_t *list);
 void insertion_sort_list(listint_t **list);
